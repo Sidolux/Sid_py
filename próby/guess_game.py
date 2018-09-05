bdcda15f5055884e3e68d7d88ea6e8c57bded5eb
Sidolux <midas78@gmail.com>
2018-09-05 18:37:14 +0200

    Guess game v1.0 - kosmetyka

--- próby/guess_game.py
+++ próby/guess_game.py
@@ -3,7 +3,7 @@ from random import randint, seed
 seed()  # seed random generator
 number = randint(1, 100)  # generate number 1-100
 # initiate vars
-guess = None
+usr_guess = None
 guess_count = 1
 diff_old = 0
 ##print(number) # for debug only
@@ -15,22 +15,22 @@ print('Aby zakończyć grę napisz quit')
 print('Zaczynajmy')
 print('----------------------------------------------------------------------------------------------')
 # Main game loop
-while not guess == number:  # play until player guesses the number
+while not usr_guess == number:  # play until player guesses the number
     usr_input = input('Podaj liczbę całkowitą 1-100: ')
     try:
-        guess = int(usr_input)  # get player input
+        usr_guess = int(usr_input)  # get player input
     except:
         if str(usr_input).lower() == 'quit':
             break
         print('To nie jest liczba całkowita')  # check if player input is number
         continue
-    if guess == number:
+    if usr_guess == number:
         print(f'Gratuluję, zgadłeś w {guess_count} próbach!!!')  # Show congratulations with guesses count
         break
-    if guess < 1 or guess > 100:  # check if player input is in range (1-100)
+    if usr_guess < 1 or usr_guess > 100:  # check if player input is in range (1-100)
         print('Podałeś liczbę spoza zakresu')
         continue
-    diff_current = abs(guess - number)  # calculate difference between player input and generated number
+    diff_current = abs(usr_guess - number)  # calculate difference between player input and generated number
     if guess_count == 1:  # on first try give hint WARM if guess is within 10, COLD if further than 10
         if diff_current <= 10:
             print('Ciepło')
