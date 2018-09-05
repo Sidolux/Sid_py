60d738a28544c954c1d313dbde8ea768b28d7bc1
Sidolux <midas78@gmail.com>
2018-09-05 18:07:59 +0200

    Guess game v0.8 - kosmetyka

--- próby/guess_game.py
+++ próby/guess_game.py
@@ -2,6 +2,7 @@ from random import randint, seed
 
 seed()  # seed random generator
 number = randint(1, 100)  # generate number 1-100
+# initiate vars
 guess = None
 guess_count = 1
 diff_old = 0
@@ -19,16 +20,16 @@ while not guess == number:  # play until player guesses the number
         print('Podałeś liczbę spoza zakresu')
         continue
     diff_current = abs(guess - number)  # calculate difference between player input and generated number
-    if guess_count <= 1: # on first try give hint WARM if guess is within 10, COLD if further than 10
+    if guess_count == 1:  # on first try give hint WARM if guess is within 10, COLD if further than 10
         if diff_current <= 10:
             print('Ciepło')
         elif diff_current > 10:
             print('Zimno')
     else:
-        if diff_current < diff_old: # on all subsequent tries give WARMER/COLDER hint
+        if diff_current < diff_old:  # on all subsequent tries give WARMER/COLDER hint
             print('Cieplej')
         elif diff_current > diff_old:
             print('Zimniej')
-    diff_old = diff_current # store old difference
-    guess_count += 1 # increment guess count
-print(f'Gratuluję, zgadłeś w {guess_count} próbach!!!') # Show congratulations with guesses count
+    diff_old = diff_current  # store old difference
+    guess_count += 1  # increment guess count
+print(f'Gratuluję, zgadłeś w {guess_count} próbach!!!')  # Show congratulations with guesses count
